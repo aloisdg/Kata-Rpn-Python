8c122136a8908a6056b9853016d9aff95e8e4af8
aloisdg <aloisdegouvello@live.fr>
2018-02-23 10:07:17 +0100

    fix input

--- polish.py
+++ polish.py
@@ -1,22 +1,13 @@
 import unittest
 
-def parseIntput(input):
-	return input;
+def parseInput(input):
+	return input
 
 # Here's our "unit tests".
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
         self.assertEqual(parseInput(1), 1)
-
-    def testTwo(self):
-        self.assertFalse(IsOdd(2))
-
-    def testException(self):
-        self.assertRaises(ValueError, DivideByTwo, 0)
-
-    def test(self):
-        self.assertEqual(1, DivideByTwo(2))
 
 def main():
     unittest.main()
