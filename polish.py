bca7c1aca6449e817093c0cc3b4aaef07a6d965b
aloisdg <aloisdegouvello@live.fr>
2018-02-23 10:10:08 +0100

    fix input

--- polish.py
+++ polish.py
@@ -1,13 +1,16 @@
 import unittest
 
 def parseInput(input):
+	if " " not in input:
+	    return input
 	return input
+
 
 # Here's our "unit tests".
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
-        self.assertEqual(parseInput(1), 1)
+        self.assertEqual(parseInput("1"), "1")
 
 def main():
     unittest.main()
