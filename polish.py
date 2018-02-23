7d8202c62b70609b5763efcbedc2b654e5ad42e8
aloisdg <aloisdegouvello@live.fr>
2018-02-23 10:04:24 +0100

    add input

--- polish.py
+++ polish.py
@@ -1,19 +1,13 @@
 import unittest
 
-# Here's our "unit".
-def IsOdd(n):
-    return n % 2 == 1
-
-def DivideByTwo(n):
-    if (n == 0):
-        raise ValueError("Zero")
-    return n / 2
+def parseIntput(input):
+	return input;
 
 # Here's our "unit tests".
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
-        self.assertTrue(IsOdd(1))
+        self.assertEqual(parseInput(1), 1)
 
     def testTwo(self):
         self.assertFalse(IsOdd(2))
