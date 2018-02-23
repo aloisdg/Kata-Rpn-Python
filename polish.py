f15608cab1b18f57465a533dd98e7106e94a045d
aloisdg <aloisdegouvello@live.fr>
2018-02-23 10:42:18 +0100

    fix stack

--- polish.py
+++ polish.py
@@ -1,4 +1,12 @@
 import unittest
+import math
+import operator
+
+ops = {'+': operator.add,
+       '-': operator.sub,
+       '*': operator.mul,
+       #'/': operator.div,
+       '^': operator.pow}
 
 def eval(inputs):
 	stack = []
@@ -6,7 +14,11 @@ def eval(inputs):
 	for i in inputs:
 		if i.isdigit():
 			stack.insert(0, int(i))
-	result = sum(stack)
+		else:
+			a = int(stack.pop(1))
+			b = int(stack.pop(0))
+			result = ops[i](a, b)
+			stack.insert(0, result)
 	return result
 
 def parseInput(input):
@@ -19,8 +31,20 @@ class IsOddTests(unittest.TestCase):
     def testOne(self):
         self.assertEqual(parseInput("1"), "1")
 
-    def testOneSpaceTab(self):
-        self.assertEqual(parseInput("1 2"), 3)
+    def testAdd(self):
+        self.assertEqual(parseInput("1 2 +"), 3)
+
+    def testSub(self):
+        self.assertEqual(parseInput("2 1 -"), 1)
+
+    def testMul(self):
+        self.assertEqual(parseInput("1 2 *"), 2)
+
+   # def testDiv(self):
+    #    self.assertEqual(parseInput("2 1 /"), 2)
+
+    def testPow(self):
+        self.assertEqual(parseInput("2 1 ^"), 2)
 
 def main():
     unittest.main()
