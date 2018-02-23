20c3cb524f00a2bb16e2d12b1abec29672e3a2e8
aloisdg <aloisdegouvello@live.fr>
2018-02-23 10:15:02 +0100

    fix input

--- polish.py
+++ polish.py
@@ -3,12 +3,15 @@ import unittest
 def parseInput(input):
 	if " " not in input:
 	    return input
-	return input
+	return input.split(' ')[0]
 
 class IsOddTests(unittest.TestCase):
 
     def testOne(self):
         self.assertEqual(parseInput("1"), "1")
+
+    def testOneSpace(self):
+        self.assertEqual(parseInput("1 2"), "1")
 
 def main():
     unittest.main()
